c67f01f6c7d89bc904565c93be8c4e06ce96297b
Avoy Das Rahul <145771204+avoy-das@users.noreply.github.com>
2024-02-15 01:05:01 +0600

    Add files via upload

---
+++ 236A.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s[1000];
+    int i,count=0,repeat[256]= {0};
+    fgets(s, sizeof(s), stdin);
+    for (i=0; i<strlen(s); i++)
+    {
+        if ( s[i]>='a' && s[i]<='z' && repeat[s[i]]==0)
+        {
+            count++;
+            repeat[s[i]]=1;
+        }
+    }
+    if (count%2==0)
+    {
+        printf("CHAT WITH HER!\n");
+    }
+    else
+    {
+        printf("IGNORE HIM!\n");
+    }
+
+    return 0;
+}

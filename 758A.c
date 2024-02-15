39fb6e2d139319195a6a8be81bf1c5f17a3622e8
Avoy Das Rahul <145771204+avoy-das@users.noreply.github.com>
2024-02-15 15:56:37 +0600

    Add files via upload

---
+++ 758A.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],sum=0;
+    for (int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int max = a[0];
+    for (int i=0;i<n;i++)
+    {
+        if (a[i]>max)
+        {
+            max = a[i];
+        }
+    }
+    for (int i=0;i<n;i++)
+    {
+        sum+=(max-a[i]);
+    }
+    printf("%d",sum);
+    return 0;
+}
